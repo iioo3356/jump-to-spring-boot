83071bdf9c2cbf3f0c4e43974096ce2938dbd5a0
iioo3356 <iioo3356@gmail.com>
2025-02-06 15:30:37 +0900

    3-11 앵커 기능 추가

--- src/main/java/com/mysite/sbb/answer/AnswerService.java
+++ src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -15,17 +15,18 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class AnswerService {
 	private final AnswerRepository answerRepository;
-	
-	public void create(Question question, String content, SiteUser author) {
+
+	public Answer create(Question question, String content, SiteUser author) {
 		Answer answer = new Answer();
 		answer.setContent(content);
 		answer.setCreateDate(LocalDateTime.now());
 		answer.setQuestion(question);
 		answer.setAuthor(author);
 		this.answerRepository.save(answer);
-		
+		return answer;
+
 	}
-	
+
 	public Answer getAnswer(Integer id) {
 		Optional<Answer> answer = this.answerRepository.findById(id);
 		if (answer.isPresent()) {
@@ -34,17 +35,17 @@ public class AnswerService {
 			throw new DataNotFoundException("answer not found");
 		}
 	}
-	
+
 	public void modify(Answer answer, String content) {
 		answer.setContent(content);
 		answer.setModifyDate(LocalDateTime.now());
 		this.answerRepository.save(answer);
 	}
-	
+
 	public void delete(Answer answer) {
 		this.answerRepository.delete(answer);
 	}
-	
+
 	public void vote(Answer answer, SiteUser siteUser) {
 		answer.getVoter().add(siteUser);
 		this.answerRepository.save(answer);
